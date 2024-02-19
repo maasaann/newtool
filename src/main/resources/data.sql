696d6a4a4ca60717360d177bb481be6b729af516
yoshitoshi.​miyata <meyutana@gmail.com>
2024-02-19 19:45:56 +0900

    ああ

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,7 @@
 INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"1のaaa、タイトル","aaa、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (CURRENT_TIMESTAMP,"(初期分)アカウント名AAA","（初期分）キーワード1",100001,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"2のbbb、タイトル","bbb、内容",2,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (CURRENT_TIMESTAMP,"(初期分)アカウント名BBB","（初期分）キーワード2",100001,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"1のccc、タイトル","ccc、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"4のddd、タイトル","ddd、内容",4,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"1のeee、タイトル","eee、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"6のfff、タイトル","fff、内容",6,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"1のggg、タイトル","ggg、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"8のhhh、タイトル","hhh、内容",8,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
-INSERT INTO daily_report_system.reports(report_date,title,content,employee_code,delete_flg,created_at,updated_at)
-     VALUES (CURRENT_TIMESTAMP,"1のiii、タイトル","iii、内容",1,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+     VALUES (CURRENT_TIMESTAMP,"(初期分)アカウント名CCC","（初期分）キーワード3",100002,0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 ;
