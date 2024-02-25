e2a0666dff7ac1aae6785baa2525fc199a372ebf
yoshitoshi.​miyata <meyutana@gmail.com>
2024-02-25 14:44:06 +0900

    2024年2月25日午後2時44分

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +32,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
-//@RequestMapping("reports")
 public class ReportController {
 
     private final ReportService reportService;
@@ -47,36 +45,118 @@ public class ReportController {
         this.reportRepository = reportRepository;
     }
 
-    // 初期の空画面
-    @GetMapping
-    public String getList1(Model model) {
+    // index
+    @GetMapping(value = "/")
+    public String getListPage(
+            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false) String order,
+            Model model) throws Exception {
 
-        int allDataCount = reportService.findAll().size();
-
-        model.addAttribute("listSize", allDataCount);
-
+        // 表示データ取得
         List<Report> reportList = reportService.findAll();
 
-        int start =   0;
-        int end   =   5;
+        // 全データ数 数える
+        int allDataCount = reportList.size();
+        model.addAttribute("listSize", allDataCount);
 
-        if ( allDataCount <= end ) {
-             end = allDataCount;
+        int start = ( 5 * page ) - 5;
+        int end   =   5 * page;
+        if ( allDataCount <= end ) { end = allDataCount; }
+
+        // 昇順 or 降順 or null
+               if( "asc".equals(order) ) {
+
+            Comparator<Report> asc = Comparator.comparing(Report::getEmployeeCode);
+            List<Report> pagedItems = reportList.stream().sorted(asc).collect(Collectors.toList()).subList(start, end);
+
+            model.addAttribute("reportList", pagedItems);
+
+        } else if( "desc".equals(order) ) {
+
+            Comparator<Report> desc = Comparator.comparing(Report::getEmployeeCode).reversed();
+            List<Report> pagedItems = reportList.stream().sorted(desc).collect(Collectors.toList()).subList(start, end);
+
+            model.addAttribute("reportList", pagedItems);
+
+        } else {
+
+            List<Report> pagedItems = reportList.subList(start, end);
+
+            model.addAttribute("reportList", pagedItems);
         }
+
+        // ページング情報格納
+        int startPage = 1;
+        int endPage   = ( allDataCount / 5 ) + 1;
+        if ( allDataCount <= endPage ) { endPage = allDataCount; }
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        // 遷移先ページ数取得
+        model.addAttribute("activePage", page);
+
+        model.addAttribute("order", order);
+
+        return "r-list";
+    }
+
+    // アカウントIDから一覧取得
+    @PostMapping(value = "/search/" )
+    public String getByCode(
+            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false) String empcode,
+            HttpServletRequest request, Model model) throws Exception {
+
+        // 空なら終了
+        if ( empcode.isEmpty() ) { return "redirect:/"; }
+
+        // 表示データ取得
+        List<Report> reportList = reportService.findByEmployeeCode(empcode);
+
+        // 検索結果 データ数 数える
+        int dataCount = reportList.size();
+
+        int start = ( 5 * page ) - 5;
+        int end   =   5 * page;
+        if ( dataCount <= end ) { end = dataCount; }
 
         List<Report> pagedItems = reportList.subList(start, end);
 
+        model.addAttribute("empcode", empcode);
+        model.addAttribute("listSize", dataCount);
         model.addAttribute("reportList", pagedItems);
 
+        // ページング情報格納
+        int startPage = 1;
+        int endPage   = ( dataCount / 5 ) + 1;
+        if ( dataCount <= endPage ) { endPage = dataCount; }
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        // 遷移先ページ数取得
+        model.addAttribute("acPage", page);
+
         return "r-list";
+    }
+
+    // アカウントID絞込時 ページング
+    @GetMapping(value = "/search/" )
+    public String getByCode2(
+            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false) String empcode,
+            HttpServletRequest request, Model model) throws Exception {
+
+        return getByCode(page, empcode, request, model);
     }
 
     // 詳細画面
     @GetMapping(value = "/r-detail/{id}/")
     public String detail(@PathVariable String id,Model model) {
-        
+
         model.addAttribute("report", reportService.findByCode(id));
-        
+
         return "r-detail";
     }
 
@@ -84,90 +164,64 @@ public class ReportController {
     @PostMapping(value = "/r-detail/{id}/update")
     public String update(
             @PathVariable String id,Report report,Model model) {
-        
+
         model.addAttribute("report", reportService.update(id, report));
 
-        model.addAttribute("listSize", reportService.findAll().size());
-        model.addAttribute("reportList", reportService.findAll());
-        
-        return "r-list";
+        return "redirect:/";
     }
 
     // 削除
     @GetMapping(value = "/delete/{id}/")
     public String delete(
             @PathVariable String id,Report report,Model model) {
-        
+
         model.addAttribute("report", reportService.delete(id, report));
 
-        model.addAttribute("listSize", reportService.findAll().size());
-        model.addAttribute("reportList", reportService.findAll());
-        
-        return "r-list";
+        return "redirect:/";
     }
 
     // 全件取得
     @GetMapping(value = "/all/")
-    public String getList2(Model model) {
+    public String getAllList(Model model) {
 
-        int allDataCount = reportService.findAll().size();
-
-        model.addAttribute("listSize", allDataCount);
-
-        List<Report> reportList = reportService.findAll();
-
-        int start =   0;
-        int end   =   5;
-
-        if ( allDataCount <= end ) {
-             end = allDataCount;
-        }
-
-        List<Report> pagedItems = reportList.subList(start, end);
-
-        model.addAttribute("reportList", pagedItems);
-
-        return "r-list";
-    }
-
-    // ページング
-    @GetMapping(value = "/{page}/")
-    public String getListPage(@PathVariable("page") Integer page, Model model) {
-
-        int allDataCount = reportService.findAll().size();
-
-        model.addAttribute("listSize", allDataCount);
-
-        List<Report> reportList = reportService.findAll();
-
-        int start = ( 5 * page ) - 5;
-        int end   =   5 * page;
-
-        if ( allDataCount <= end ) {
-             end = allDataCount;
-        }
-
-        List<Report> pagedItems = reportList.subList(start, end);
-
-        model.addAttribute("reportList", pagedItems);
-
-        return "r-list";
+        return "redirect:/";
     }
 
     // 昇順処理
     @GetMapping(value = "/asc/")
-    public String orderByAsc(Model model) {
+    public String orderByAsc(
+            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false) String order,
+            Model model) {
+
+        // 全データ取得
+        List<Report> reportList = reportService.findAll();
 
         // 全データ数
-        int allDataCount = reportService.findAll().size();
+        int allDataCount = reportList.size();
         model.addAttribute("listSize", allDataCount);
 
-        // 昇順処理
-        List<Report> reportList = reportService.findAll();
+        int start = ( 5 * page ) - 5;
+        int end   =   5 * page;
+        if ( allDataCount <= end ) { end = allDataCount; }
+
+        // ここで昇順処理
         Comparator<Report> asc = Comparator.comparing(Report::getEmployeeCode);
         List<Report> ascList = reportList.stream().sorted(asc).collect(Collectors.toList());
 
-        model.addAttribute("reportList", ascList);
+        // 1ページ目のみを格納
+        List<Report> pagedItems = ascList.subList(start, end);
+        model.addAttribute("reportList", pagedItems);
+
+        // ページング情報格納
+        int startPage = 1;
+        int endPage   = ( allDataCount / 5 ) + 1;
+        if ( allDataCount <= endPage ) { endPage = allDataCount; }
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        model.addAttribute("activePage", 1);
 
         return "r-list";
     }
@@ -187,21 +241,15 @@ public class ReportController {
 
         model.addAttribute("reportList", descList);
 
-        return "r-list";
-    }
+        // ページング情報格納
+        int startPage = 1;
+        int endPage   = ( allDataCount / 5 ) + 1;
+        if ( allDataCount <= endPage ) { endPage = allDataCount; }
 
-    // アカウントIDから一覧取得
-    @PostMapping(value = "/search/" )
-    public String getByCode(@RequestParam("empcode") String empcode,
-            HttpServletRequest request, Model model) {
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
-        if ( empcode.isEmpty() ) {
-            return "redirect:/";
-        }
-
-        model.addAttribute("empcode", empcode);
-        model.addAttribute("listSize", reportService.findByEmployeeCode(empcode).size());
-        model.addAttribute("reportList", reportService.findByEmployeeCode(empcode));
+        model.addAttribute("activePage", 1);
 
         return "r-list";
     }
@@ -301,7 +349,7 @@ public class ReportController {
             return "redirect:/";
         }
 
-        return "redirect:/all/";
+        return "redirect:/";
     }
 
 }
